8b1eac446ad5b22e5850cae226933ec019913ba8
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2023-02-13 23:30:30 +0200

    [features-oldCryptoRewrite]

--- src/App.vue
+++ src/App.vue
@@ -5,35 +5,43 @@ export default {
   data() {
     return {
       tickerNameInput: "",
-      tickers: [{ name: "test", price: "-" }],
+      tickers: [{ name: "BTC", price: "-" }],
       selectedTicker: null,
-      coinsToRecommend: [1,2,3,4],
+      coinsToRecommend: [],
+      coins: null,
+      tickerIsAlreadyExist: false,
     };
-  },
-
-  nonReactData: {
-    coins: null, // replace to data
   },
 
   created() {
     const cryptoCoins = async () => {
-      const f = await fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true');
+      const f = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?summary=true"
+      );
       const result = await f.json();
-      this.$options.nonReactData.coins = result.Data;
-    }
+      this.coins = result.Data;
+    };
     cryptoCoins();
   },
 
   methods: {
     addNewTicker() {
-      if (this.tickerNameInput !== "") {
-        this.tickers.push({
-          name: this.tickerNameInput.toUpperCase(),
-          price: "-",
-        });
+      const newTicker =
+        this.tickerNameInput === ""
+          ? ""
+          : this.tickers.every(
+              (item) =>
+                item.name.toLowerCase() !== this.tickerNameInput.toLowerCase()
+            )
+          ? { name: this.tickerNameInput.toUpperCase(), price: "-" }
+          : (this.tickerIsAlreadyExist = true) && "";
+
+      newTicker !== "" ? this.tickers.push(newTicker) && (this.tickerIsAlreadyExist = false) : "";
+
+      if(!this.tickerIsAlreadyExist){
         this.tickerNameInput = "";
+        this.coinsToRecommend = [];
       }
-      console.log(this.$options.nonReactData.coins)
     },
 
     removeTicker(tickerToRemove) {
@@ -41,10 +49,18 @@ export default {
       this.selectedTicker = null;
     },
 
-    recommendCoins(){
-      console.log(this.$options.nonReactData.coins)
-
-    }
+    recommendCoins() {
+      this.coinsToRecommend = [];
+      for (const coin of Object.keys(this.coins)) {
+        if (
+          coin.toLowerCase().includes(this.tickerNameInput.toLowerCase()) &&
+          this.coinsToRecommend.length < 4 &&
+          this.tickerNameInput !== ""
+        ) {
+          this.coinsToRecommend.push(coin);
+        }
+      }
+    },
   },
 };
 </script>
@@ -67,20 +83,26 @@ export default {
                 placeholder="Например DOGE"
                 v-model="tickerNameInput"
                 @keydown.enter="addNewTicker"
+                @input="recommendCoins(), tickerIsAlreadyExist = false"
               />
             </div>
-            <div
-              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
-            >
-              <span
-                v-for="coin in coinsToRecommend"
-                v-bind:key="coin"
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+            <template v-if="coinsToRecommend.length">
+              <div
+                class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
               >
-                BTC
-              </span>
-            </div>
-            <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
+                <span
+                  v-for="coin in coinsToRecommend"
+                  v-bind:key="coin"
+                  @click="tickerNameInput = coin, addNewTicker()"
+                  class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+                >
+                  {{ coin }}
+                </span>
+              </div>
+            </template>
+            <template v-if="tickerIsAlreadyExist">
+              <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
+            </template>
           </div>
         </div>
         <button
@@ -88,7 +110,6 @@ export default {
           type="button"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
-          <!-- Heroicon name: solid/mail -->
           <svg
             class="-ml-0.5 mr-2 h-6 w-6"
             xmlns="http://www.w3.org/2000/svg"
@@ -109,11 +130,11 @@ export default {
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-              class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-              v-for="ticker in tickers"
-              v-bind:key="ticker"
-              @click="selectedTicker = ticker"
-              v-bind:class="selectedTicker === ticker ? 'border-4' : ''"
+            class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+            v-for="ticker in tickers"
+            v-bind:key="ticker"
+            @click="selectedTicker = ticker"
+            v-bind:class="selectedTicker === ticker ? 'border-4' : ''"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
@@ -125,20 +146,20 @@ export default {
             </div>
             <div class="w-full border-t border-gray-200"></div>
             <button
-                @click.stop="removeTicker(ticker)"
-                class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+              @click.stop="removeTicker(ticker)"
+              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
               <svg
-                  class="h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="#718096"
-                  aria-hidden="true"
+                class="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="#718096"
+                aria-hidden="true"
               >
                 <path
-                    fill-rule="evenodd"
-                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                    clip-rule="evenodd"
+                  fill-rule="evenodd"
+                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                  clip-rule="evenodd"
                 ></path></svg
               >Удалить
             </button>
@@ -148,7 +169,7 @@ export default {
         <template v-if="selectedTicker">
           <section class="relative">
             <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-              {{selectedTicker.name}} - USD
+              {{ selectedTicker.name }} - USD
             </h3>
             <div class="flex items-end border-gray-600 border-b border-l h-64">
               <div class="bg-purple-800 border w-10 h-24"></div>
@@ -156,25 +177,29 @@ export default {
               <div class="bg-purple-800 border w-10 h-48"></div>
               <div class="bg-purple-800 border w-10 h-16"></div>
             </div>
-            <button type="button" class="absolute top-0 right-0" @click="selectedTicker = null">
+            <button
+              type="button"
+              class="absolute top-0 right-0"
+              @click="selectedTicker = null"
+            >
               <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  x="0"
-                  y="0"
-                  viewBox="0 0 511.76 511.76"
-                  style="enable-background: new 0 0 512 512"
-                  xml:space="preserve"
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                x="0"
+                y="0"
+                viewBox="0 0 511.76 511.76"
+                style="enable-background: new 0 0 512 512"
+                xml:space="preserve"
               >
-            <g>
-              <path
-                  d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                  fill="#718096"
-                  data-original="#000000"
-              ></path>
-            </g>
-          </svg>
+                <g>
+                  <path
+                    d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                    fill="#718096"
+                    data-original="#000000"
+                  ></path>
+                </g>
+              </svg>
             </button>
           </section>
         </template>
