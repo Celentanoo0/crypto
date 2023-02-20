b39838202ef6c9fd4a618b3dbb8c12b9528d6bd9
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2023-02-20 22:23:31 +0200

    todo comment

--- src/api.js
+++ src/api.js
@@ -1,3 +1,6 @@
+//todo:
+// fix the problem when you're opening app in another tab it isn't working. With broadcast channel api or ls
+
 const API_KEY =
   "a0ed06110a6de56d8c55fc29917fcff11c8f5bd60373178e5b98259ca8356f10";
 
