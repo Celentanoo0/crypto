ad1145e7fbe209d3257e331b8cfb4d79bc71e2b4
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2023-02-25 20:12:43 +0200

    sharedWorker for websocet

--- src/api.js
+++ src/api.js
@@ -1,58 +1,34 @@
-//todo:
-// fix the problem when you're opening app in another tab it isn't working. With broadcast channel api or ls
-
-const API_KEY =
-  "a0ed06110a6de56d8c55fc29917fcff11c8f5bd60373178e5b98259ca8356f10";
+const worker = new SharedWorker('src/sharedWorker.js');
+worker.port.start();
 
 const tickersHandlers = new Map();
 
-const socket = new WebSocket(
-  `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
-);
-
 const subscribeToWS = (tickerName) => {
-  sendMessage({
+  worker.port.postMessage({
     action: "SubAdd",
     subs: [`5~CCCAGG~${tickerName}~USD`],
   });
 };
 
 const unsubscribeFromWS = (tickerName) => {
-  sendMessage({
+  worker.port.postMessage({
     action: "SubRemove",
     subs: [`5~CCCAGG~${tickerName}~USD`],
   });
 };
 
 const subscribeToWSForNonTradablePairs = (tickerName) => {
-  sendMessage({
+  worker.port.postMessage({
     action: "SubAdd",
     subs: [`5~CCCAGG~${tickerName}~BTC`],
   });
 };
 
 const unsubscribeFromWSForNonTradablePairs = (tickerName) => {
-  sendMessage({
+  worker.port.postMessage({
     action: "SubRemove",
     subs: [`5~CCCAGG~${tickerName}~BTC`],
   });
-};
-
-const sendMessage = (message) => {
-  const messageToSend = JSON.stringify(message);
-
-  if (socket.readyState === WebSocket.OPEN) {
-    socket.send(messageToSend);
-    return;
-  }
-
-  socket.addEventListener(
-    "open",
-    () => {
-      socket.send(messageToSend);
-    },
-    { once: true }
-  );
 };
 
 const CURRENCY_INDEX = "5";
@@ -60,7 +36,7 @@ const ERROR_INDEX = "500";
 let btcToUsd = 1;
 let cryptoPairIsAlreadyExist = tickersHandlers.has("BTC");
 
-socket.addEventListener("message", (e) => {
+worker.port.addEventListener("message", (e) => {
   const {
     TYPE: type,
     PRICE: price,
